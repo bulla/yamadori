c827b12ad5129d1d91ed1bad14738ae0a318e825
Alex Bulla <abulla@openwt.com>
2020-11-15 12:17:13 +0100

    Initialized Java project with Spring Boot Initializr.

---
+++ src/main/java/app/yamadori/backend/YamadoriApplication.java
@@ -0,0 +1,13 @@
+package app.yamadori.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamadoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamadoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/yamadori/backend/YamadoriApplicationTests.java
@@ -0,0 +1,13 @@
+package app.yamadori.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamadoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
